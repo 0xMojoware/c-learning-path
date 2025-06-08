90d26a580c33961449ca76ea2798a3fe5f6eed96
redmojo <redmojo@redmojo.redmojo>
2025-06-08 09:03:21 -0400

    Protostar Stack 3

---
+++ protostar/stack3/Stackthree.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <err.h>
+
+extern char *gets(char *);
+
+void win()
+{
+  printf("code flow successfully changed\n");
+}
+
+int main(int argc, char **argv)
+{
+  volatile int (*fp)();
+  char buffer[64];
+
+  fp = 0;
+
+  gets(buffer);
+
+  if(fp) {
+      printf("calling function pointer, jumping to 0x%08x\n", fp);
+      fp();
+  }
+}
